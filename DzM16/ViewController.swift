937e92002899f308d5121c12e60db8e718d8e6f1
Vasiliy <vygnych110197@icloud.com>
2023-02-26 15:10:34 +0300

    all files this project

---
+++ DzM16/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DzM16
+//
+//  Created by Vasiliy Vygnych on 24.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        
+    }
+
+
+}
+
