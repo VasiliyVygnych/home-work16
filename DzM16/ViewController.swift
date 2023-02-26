8556e79de7aac2894869d297eb1b4b26e40b3b17
Vasiliy <vygnych110197@icloud.com>
2023-02-26 17:08:40 +0300

    comitt

--- DzM16/ViewController.swift
+++ DzM16/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .red
         view.addSubview(label)
         
     }
