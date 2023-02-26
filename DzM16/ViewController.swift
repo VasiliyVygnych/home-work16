ffc14ec2bc861c6d4471e6883aac3f0a753be758
Vasiliy <vygnych110197@icloud.com>
2023-02-26 15:19:15 +0300

    add label

--- DzM16/ViewController.swift
+++ DzM16/ViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var label: UILabel = {
+       let label = UILabel(frame: CGRect(x: 50 , y: 50, width: 50, height: 50))
+        label.text = "Hello"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .blue
+        view.addSubview(label)
         
     }
 
