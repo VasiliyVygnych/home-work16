62ec5e4ab9ce36fccb03c6ef1c3c5cb04ac596c9
Vasiliy <vygnych110197@icloud.com>
2023-02-26 17:28:45 +0300

    change view color and text label

--- DzM16/ViewController.swift
+++ DzM16/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     private lazy var label: UILabel = {
        let label = UILabel(frame: CGRect(x: 50 , y: 50, width: 50, height: 50))
-        label.text = "Hello"
+        label.text = "Привет"
         label.textColor = .black
         label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
         return label
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .blue
         view.addSubview(label)
         
     }
